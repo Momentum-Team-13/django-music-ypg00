50c789b4b9ad649b52c96d03116909d35445a5ad
Yuriy Golota <ypgolota@pm.me>
2022-06-30 15:00:00 -0400

    added __str__ methods for Album and Artist models; tested on Admin site, was able to create both

--- albums/models.py
+++ albums/models.py
@@ -3,8 +3,14 @@ from django.db import models
 class Album(models.Model):
     title = models.CharField(max_length=255)
     artist = models.CharField(max_length=255)
-    created_at = models.DateTimeField() # research how to autofill
+    # created_at = models.DateTimeField() # research how to autofill
+
+    def __str__(self):
+        return f'{self.title}'
 
 class Artist(models.Model):
     name = models.CharField(max_length=255)
-    albums = models.ForeignObject()
+    # albums = models.ForeignObject()
+
+    def __str__(self):
+        return f'{self.name}'
