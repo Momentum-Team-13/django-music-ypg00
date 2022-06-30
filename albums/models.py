9dc20fcd22593c605f14035760453d25b51179ef
Yuriy Golota <ypgolota@pm.me>
2022-06-30 14:51:09 -0400

    added models to admin.py

--- albums/models.py
+++ albums/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Album(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    created_at = models.DateTimeField() # research how to autofill
+
+class Artist(models.Model):
+    name = models.CharField(max_length=255)
+    albums = models.ForeignObject()
