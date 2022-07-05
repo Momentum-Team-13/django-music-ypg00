03d86901ca33213f33b80aa1af2ce0bbe982239e
Yuriy Golota <ypgolota@pm.me>
2022-07-05 13:05:39 -0400

    updated models.py for two many-to-many relationships between albums and artists

--- albums/models.py
+++ albums/models.py
@@ -2,15 +2,16 @@ from django.db import models
 
 class Album(models.Model):
     title = models.CharField(max_length=255)
-    artists = models.CharField(max_length=255, null=True, blank=True)
+    artists = models.ManyToManyField('Artist')
     created_at = models.DateTimeField(auto_now_add=True)
 
-    # def __str__(self):
-    #     return f'{self.title}'
+    def __str__(self):
+        return self.title
 
 class Artist(models.Model):
     artist_name = models.CharField(max_length=255)
-    albums = models.ForeignKey('Album', on_delete=models.CASCADE, related_name='artist_name', null=True, blank=True)
+    albums = models.ManyToManyField(Album)
 
-    # def __str__(self):
-    #     return f'{self.name}'
+    def __str__(self):
+        return self.artist_name
+
--- albums/views.py
+++ albums/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Album
 
 def list_albums(request):
-    return render(request, 'albums/list_albums.html')
+    albums = Album.objects.all()
+    return render(request, 'albums/list_albums.html',{'albums': albums})
 
 def create_album(request):
     pass
