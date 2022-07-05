177a69ea6fece5413b5a1f6925f5336bcdb5a79f
Yuriy Golota <ypgolota@pm.me>
2022-07-05 13:44:32 -0400

    updated models to include many to many and one to many, debugging now

--- albums/models.py
+++ albums/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class Album(models.Model):
     title = models.CharField(max_length=255)
-    artists = models.ManyToManyField('Artist', null=True, blank=True)
+    artist = models.ForeignKey('Artist', on_delete=models.CASCADE, related_name='title', null=True, blank=True)
+    # artist = models.ManyToManyField('Artist', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -10,7 +11,8 @@ class Album(models.Model):
 
 class Artist(models.Model):
     artist_name = models.CharField(max_length=255)
-    albums = models.ManyToManyField(Album, null=True, blank=True)
+    album = models.ForeignKey('Album', on_delete=models.CASCADE, related_name='artist_name', null=True, blank=True)
+    # album = models.ManyToManyField(Album, null=True, blank=True)
 
     def __str__(self):
         return self.artist_name
