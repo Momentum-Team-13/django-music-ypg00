217cc036856974739d27ad2b4abc496f74a25df6
Yuriy Golota <ypgolota@pm.me>
2022-07-05 14:19:33 -0400

    corrected many to many relationship btw albums and artists

--- albums/models.py
+++ albums/models.py
@@ -2,17 +2,15 @@ from django.db import models
 
 class Album(models.Model):
     title = models.CharField(max_length=255)
-    artist = models.ForeignKey('Artist', on_delete=models.CASCADE, related_name='title', null=True, blank=True)
-    # artist = models.ManyToManyField('Artist', null=True, blank=True)
+    # artist = models.ForeignKey('Artist', on_delete=models.CASCADE, related_name='title', null=True, blank=True)
+    artists = models.ManyToManyField('Artist', related_name='albums', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
 
 class Artist(models.Model):
-    artist_name = models.CharField(max_length=255)
-    album = models.ForeignKey('Album', on_delete=models.CASCADE, related_name='artist_name', null=True, blank=True)
-    # album = models.ManyToManyField(Album, null=True, blank=True)
+    name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.artist_name
